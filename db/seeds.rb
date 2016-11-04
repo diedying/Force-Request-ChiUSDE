bf1927a52f35150b7d5da6de28bd6c75e6a754c5
Adil Hamid Malla <adil.hamid@tamu.edu>
2016-11-04 01:56:28 +0000

    Added the Admin View with Different Course Tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,15 @@ student_requests = [{:uin => '425008306', :full_name => 'Adil Hamid Malla',
                       {:uin => '225008989', :full_name => 'Shubham Jain', 
                       :major => 'CSE', :classification => 'A', :minor => 'Image Processing', :email => 'shubham7jain@tamu.edu', :phone => '97977979', 
                       :expected_graduation => '2018', :request_semester => 'fall',
-                      :course_id => '026', :section_id => '101', :notes => 'This force request is at number 1 in queue :P' , :state => 'active'}]
+                      :course_id => '027', :section_id => '101', :notes => 'This force request is at number 1 in queue :P' , :state => 'active'},
+                       {:uin => '225008989', :full_name => 'ShubHHHham Jain', 
+                      :major => 'CSE', :classification => 'A', :minor => 'Image Processing', :email => 'shubham7jain@tamu.edu', :phone => '97977979', 
+                      :expected_graduation => '2018', :request_semester => 'fall',
+                      :course_id => '027', :section_id => '101', :notes => 'This force request is at number 1 in queue :P' , :state => 'active'},
+                       {:uin => '225008989', :full_name => 'Shubhamkjkj Jain', 
+                      :major => 'CSE', :classification => 'A', :minor => 'Image Processing', :email => 'shubham7jain@tamu.edu', :phone => '97977979', 
+                      :expected_graduation => '2018', :request_semester => 'fall',
+                      :course_id => '028', :section_id => '101', :notes => 'This force request is at number 1 in queue :P' , :state => 'active'}]
 
 student_requests.each do |record|
   StudentRequest.create!(record)
