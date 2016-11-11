7c67e03edd4c45f7239d6cda324e9c3d895d6ac9
Shubham Jain <shubham7jain@gmail.com>
2016-11-11 04:12:56 +0000

    Added audited gem to create audit automatically and some validations on Force request creeate page

--- db/schema.rb
+++ db/schema.rb
@@ -11,32 +11,56 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150809022253) do
+ActiveRecord::Schema.define(version: 20161109212823) do
 
-  create_table "student_requests", force: :cascade do |t|
-    t.string "request_id"
-    t.string "uin"
-    t.string "full_name"
-    t.string "major"
-    t.string "classification"
-    t.string "minor"
-    t.string "email"
-    t.string "phone"
-    t.string "expected_graduation"
-    t.string "request_semester"
-    t.string "course_id"
-    t.string "section_id"
-    t.text "notes"
-    t.string "state"
-    t.string "priority"
-    t.datetime "creation_date"
-    t.datetime "last_updated"
-    t.text "notes_to_student"
-    t.text "admin_notes"
+  create_table "audits", force: :cascade do |t|
+    t.integer  "auditable_id"
+    t.string   "auditable_type"
+    t.integer  "associated_id"
+    t.string   "associated_type"
+    t.integer  "user_id"
+    t.string   "user_type"
+    t.string   "username"
+    t.string   "action"
+    t.text     "audited_changes"
+    t.integer  "version",         default: 0
+    t.string   "comment"
+    t.string   "remote_address"
+    t.string   "request_uuid"
+    t.datetime "created_at"
   end
 
-  add_index "student_requests", ["request_id"], name: "index_student_requests_on_request_id", unique: true
+  add_index "audits", ["associated_id", "associated_type"], name: "associated_index"
+  add_index "audits", ["auditable_id", "auditable_type"], name: "auditable_index"
+  add_index "audits", ["created_at"], name: "index_audits_on_created_at"
+  add_index "audits", ["request_uuid"], name: "index_audits_on_request_uuid"
+  add_index "audits", ["user_id", "user_type"], name: "user_index"
+
+  create_table "student_requests", force: :cascade do |t|
+    t.string   "request_id"
+    t.string   "uin"
+    t.string   "full_name"
+    t.string   "major"
+    t.string   "classification"
+    t.string   "minor"
+    t.string   "email"
+    t.string   "phone"
+    t.string   "expected_graduation"
+    t.string   "request_semester"
+    t.string   "course_id"
+    t.string   "section_id"
+    t.text     "notes"
+    t.string   "state"
+    t.string   "priority"
+    t.datetime "creation_date"
+    t.datetime "last_updated"
+    t.text     "notes_to_student"
+    t.text     "admin_notes"
+  end
+
   add_index "student_requests", ["course_id"], name: "index_student_requests_on_course_id"
+  add_index "student_requests", ["request_id"], name: "index_student_requests_on_request_id", unique: true
   add_index "student_requests", ["section_id"], name: "index_student_requests_on_section_id"
   add_index "student_requests", ["state"], name: "index_student_requests_on_state"
+
 end
