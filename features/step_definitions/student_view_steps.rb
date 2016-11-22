10b43201cfa8a89d6870010b5548b4c65cc86010
Shubham Jain <shubham7jain@gmail.com>
2016-11-22 22:27:53 +0000

    Fixed cucumber test cases and add coverage for cucumber also

--- features/step_definitions/student_view_steps.rb
+++ features/step_definitions/student_view_steps.rb
@@ -9,15 +9,13 @@ When(/^I click on New Force Request/) do
   click_link('New Force Request')
   @student_request = {:uin => "1234312", :full_name =>"Dummy Guy", :major=>"Computer Science", :classification=>"G8", 
   :minor=>"None", :email=>"dummy@dummy.com", :phone=>"312321312", :expected_graduation=>"2018", :request_semester=>"Fall", :course_id=>"12"}
-  fill_in('UIN', :with => @student_request[:uin])
+  # fill_in('UIN', :with => @student_request[:uin])
   fill_in('Full Name', :with => @student_request[:full_name])
   fill_in('Major', :with => @student_request[:major])
   select('G7', :from => 'Classification')
   fill_in('Minor', :with => @student_request[:minor])
   fill_in('Email', :with => @student_request[:email])
   fill_in('Phone', :with => @student_request[:phone])
-  fill_in('Expected Graduation', :with => @student_request[:expected_graduation])
-  fill_in('Request Semester', :with => @student_request[:request_semester])
   fill_in('Course Id', :with => @student_request[:uin]) 
   click_button('Save Request')
 end
@@ -30,15 +28,12 @@ When(/^I click on Withdraw$/) do
   click_link('New Force Request')
   @student_request = {:uin => "1234312", :full_name =>"Dummy Guy", :major=>"Computer Science", :classification=>"G8", 
   :minor=>"None", :email=>"dummy@dummy.com", :phone=>"312321312", :expected_graduation=>"2018", :request_semester=>"Fall", :course_id=>"12"}
-  fill_in('UIN', :with => @student_request[:uin])
   fill_in('Full Name', :with => @student_request[:full_name])
   fill_in('Major', :with => @student_request[:major])
   select('G7', :from => 'Classification')
   fill_in('Minor', :with => @student_request[:minor])
   fill_in('Email', :with => @student_request[:email])
   fill_in('Phone', :with => @student_request[:phone])
-  fill_in('Expected Graduation', :with => @student_request[:expected_graduation])
-  fill_in('Request Semester', :with => @student_request[:request_semester])
   fill_in('Course Id', :with => @student_request[:uin])  
   click_button('Save Request')
   
