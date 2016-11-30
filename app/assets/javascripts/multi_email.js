aad1120d9cab5c84fb7320aaed3d70c3cf380cbb
navneet gupta <navgupta14@gmail.com>
2016-11-30 20:45:07 +0000

    handling multiple email scenario properly

--- app/assets/javascripts/multi_email.js
+++ app/assets/javascripts/multi_email.js
@@ -1,9 +1,19 @@
 
 
 function multi_email(subject, email_list) {
+    
+    email_list = []
+    console.log(subject)
+    $("input:checkbox[name=request_ids][id=" + subject +"]:checked").each(function(){
+        email_list.push($(this).val());
+    });
+    
+    console.log(email_list)
+    alert(email_list)
     var uniq_email_list = email_list.filter(function(itm, i) {
         return email_list.indexOf(itm)== i; 
     });
     uniq_email_list.join(',');
-    window.open("mailto:" + uniq_email_list + "?subject=" + "Force Request System: Regarding course " + subject);
+    if (uniq_email_list.length >= 1)
+        window.open("mailto:" + uniq_email_list + "?subject=" + "Force Request System: Regarding course " + subject);
 }
