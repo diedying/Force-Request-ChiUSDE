985c8224a68957ceb6b2e11c059dd1a4beb30460
navneet gupta <navgupta14@gmail.com>
2016-12-01 05:43:30 +0000

    quieting arbit logs -- commited in last checkin

--- app/assets/javascripts/multi_email.js
+++ app/assets/javascripts/multi_email.js
@@ -8,11 +8,10 @@ function multi_email(subject, email_list) {
         email_list.push($(this).val());
     });
     
-    console.log(email_list)
-    alert(email_list)
     var uniq_email_list = email_list.filter(function(itm, i) {
         return email_list.indexOf(itm)== i; 
     });
+    
     uniq_email_list.join(',');
     if (uniq_email_list.length >= 1)
         window.open("mailto:" + uniq_email_list + "?subject=" + "Force Request System: Regarding course " + subject);
