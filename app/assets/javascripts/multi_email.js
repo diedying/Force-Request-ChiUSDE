9825ec3b77ba8c88ea88d8ca4c62a0bdeb424448
navneet gupta <navgupta14@gmail.com>
2016-12-01 21:43:20 +0000

    multiple email and update fix + code refactoring

--- app/assets/javascripts/multi_email.js
+++ app/assets/javascripts/multi_email.js
@@ -1,11 +1,10 @@
 
 
-function multi_email(subject, email_list) {
-    
-    email_list = []
+function multi_email(subject) {
+    var  email_list = []
     console.log(subject)
-    $("input:checkbox[name=request_ids][id=" + subject +"]:checked").each(function(){
-        email_list.push($(this).val());
+    $("input:checkbox[name=request_ids\\[\\]][sub=" + subject +"]:checked").each(function(){
+        email_list.push($(this).data("mail"));
     });
     
     var uniq_email_list = email_list.filter(function(itm, i) {
