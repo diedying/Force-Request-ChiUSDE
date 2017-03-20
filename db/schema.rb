d386df91953d930595882d5606cf59057c7324a5
Mo Li <limo9373@gmail.com>
2017-03-20 19:40:32 +0000

    iteration1.1

--- db/schema.rb
+++ db/schema.rb
@@ -99,6 +99,7 @@ ActiveRecord::Schema.define(version: 20170319213912) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
+    t.string   "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
