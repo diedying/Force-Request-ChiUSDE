888406ed17763de8b1a3c77f413cdd519bcfa85f
Leyi Hu <leyihu61@gmail.com>
2017-04-03 04:07:19 +0000

    updated email pattern to match NetID pattern

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,7 +3,7 @@ class Student < ActiveRecord::Base
     validates :email, presence: true
     validates :password, presence: true
     validates :uin, presence: true
-    validates_format_of :email, :with => /\A(\w+)@(tamu.edu)\z/i
+    # validates_format_of :email, :with => /\A(\w+)@(tamu.edu)\z/i
     validates_format_of :name, :with => /\w+/, :multiline => true
     # # attr_accessor :name, :email
 end
