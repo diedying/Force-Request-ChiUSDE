e038371987cee8bcb0203c8bde73e777f2119e81
Mo Li <limo9373@gmail.com>
2017-04-05 15:42:48 +0000

    Version beta 1.0  For Deploying

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,66 +7,65 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-student_requests = [{:uin => '425008306', :name => 'Adil Hamid Malla', 
-                     :major => 'CPSC', :classification => 'U2', :minor => 'CECN', :email => 'adil@gmail.com', :phone => '9797797697', 
-                     :expected_graduation => '2018 Spring', :request_semester => '2017 Fall',
-                     :course_id => '026', :section_id => '101', :notes => 'Requirement for graduation.',:priority => 'Very High' , :state => 'Active'},
-                     {:uin => '225008988', :name => 'Bhavik Ameta', 
-                      :major => 'CPSC', :classification => 'G7', :minor => 'MECH', :email => 'bhavik@tamu.edu', :phone => '9797769279', 
-                      :expected_graduation => '2018 Summer', :request_semester => '2017 Spring',
-                      :course_id => '026', :section_id => '101', :notes => 'I need this course as my summer intern will deal with this.',:priority => 'Very High' , :state => 'Active'},
-                      {:uin => '225008989', :name => 'Shubham Jain', 
-                      :major => 'CPSC', :classification => 'G8', :minor => 'CVEN', :email => 'shubham7jain@tamu.edu', :phone => '9793477979', 
-                      :expected_graduation => '2018 Fall', :request_semester => '2017 Summer',
-                      :course_id => '027', :section_id => '101', :notes => 'My research advisor has recommended this course.' ,:priority => 'Very High', :state => 'Active'},
-                       {:uin => '225008989', :name => 'Shubham Jain', 
-                      :major => 'CPSC', :classification => 'G8', :minor => 'MATH', :email => 'shubham7jain@tamu.edu', :phone => '9797797900', 
-                      :expected_graduation => '2017 Fall', :request_semester => '2017 Fall',
-                      :course_id => '026', :section_id => '101', :notes => 'I am interested in CS026 as it is my field of research.' ,:priority => 'Very High', :state => 'Active'},
-                       {:uin => '225008989', :name => 'Shubham Jain', 
-                      :major => 'CPSC', :classification => 'G8', :minor => 'PHYS', :email => 'shubham7jain@tamu.edu', :phone => '9792377979', 
-                      :expected_graduation => '2017 Fall', :request_semester => '2017 Spring',
-                      :course_id => '028', :section_id => '101', :notes => 'I need this subject to graduate.',:priority => 'Very High' , :state => 'Active'},
-                      {:uin => '525008931', :name => 'Aditya Nanjangud', 
-                     :major => 'CPSC', :classification => 'G8', :minor => 'CECN', :email => 'aditya@gmail.com', :phone => '9797797697', 
-                     :expected_graduation => '2018 Spring', :request_semester => '2017 Fall',
-                     :course_id => '026', :section_id => '101', :notes => 'I need this course since this is my last semester',:priority => 'Very High' , :state => 'Active'},
-                     {:uin => '525008931', :name => 'Aditya Nanjangud', 
-                     :major => 'CPSC', :classification => 'G8', :minor => 'CECN', :email => 'aditya@gmail.com', :phone => '9797797697', 
-                     :expected_graduation => '2018 Spring', :request_semester => '2017 Fall',
-                     :course_id => '027', :section_id => '101', :notes => 'I need this course since this is my last semester',:priority => 'Very High' , :state => 'Active'},
-                     {:uin => '525008931', :name => 'Aditya Nanjangud', 
-                     :major => 'CPSC', :classification => 'G8', :minor => 'CECN', :email => 'aditya@gmail.com', :phone => '9797797697', 
-                     :expected_graduation => '2018 Spring', :request_semester => '2017 Fall',
-                     :course_id => '028', :section_id => '101', :notes => 'I need this course since this is my last semester',:priority => 'Very High' , :state => 'Active'},
-                     {:uin => '525008932', :name => 'Navneet Gupta', 
-                     :major => 'CPSC', :classification => 'G8', :minor => 'CECN', :email => 'navneet@gmail.com', :phone => '4693889840', 
-                     :expected_graduation => '2018 Spring', :request_semester => '2017 Fall',
-                     :course_id => '026', :section_id => '101', :notes => 'I need this course since this is my last semester',:priority => 'High' , :state => 'Active'},
-                     {:uin => '525008932', :name => 'Navneet Gupta', 
-                     :major => 'CPSC', :classification => 'G8', :minor => 'CECN', :email => 'navneet@gmail.com', :phone => '4693889840', 
-                     :expected_graduation => '2018 Spring', :request_semester => '2017 Fall',
-                     :course_id => '027', :section_id => '101', :notes => 'I need this course since this is my last semester',:priority => 'High' , :state => 'Active'},
-                     {:uin => '525008932', :name => 'Navneet Gupta', 
-                     :major => 'CPSC', :classification => 'G8', :minor => 'CECN', :email => 'navneet@gmail.com', :phone => '4693889840', 
-                     :expected_graduation => '2018 Spring', :request_semester => '2017 Fall',
-                     :course_id => '028', :section_id => '101', :notes => 'I need this course since this is my last semester',:priority => 'High' , :state => 'Approved'}]
+# student_requests = [{:uin => '425008306', :name => 'Adil Hamid Malla', 
+#                      :major => 'CPSC', :classification => 'U2', :minor => 'CECN', :email => 'adil@gmail.com', :phone => '9797797697', 
+#                      :expected_graduation => '2018 Spring', :request_semester => '2017 Fall',
+#                      :course_id => '026', :section_id => '101', :notes => 'Requirement for graduation.',:priority => 'Very High' , :state => 'Active'},
+#                      {:uin => '225008988', :name => 'Bhavik Ameta', 
+#                       :major => 'CPSC', :classification => 'G7', :minor => 'MECH', :email => 'bhavik@tamu.edu', :phone => '9797769279', 
+#                       :expected_graduation => '2018 Summer', :request_semester => '2017 Spring',
+#                       :course_id => '026', :section_id => '101', :notes => 'I need this course as my summer intern will deal with this.',:priority => 'Very High' , :state => 'Active'},
+#                       {:uin => '225008989', :name => 'Shubham Jain', 
+#                       :major => 'CPSC', :classification => 'G8', :minor => 'CVEN', :email => 'shubham7jain@tamu.edu', :phone => '9793477979', 
+#                       :expected_graduation => '2018 Fall', :request_semester => '2017 Summer',
+#                       :course_id => '027', :section_id => '101', :notes => 'My research advisor has recommended this course.' ,:priority => 'Very High', :state => 'Active'},
+#                       {:uin => '225008989', :name => 'Shubham Jain', 
+#                       :major => 'CPSC', :classification => 'G8', :minor => 'MATH', :email => 'shubham7jain@tamu.edu', :phone => '9797797900', 
+#                       :expected_graduation => '2017 Fall', :request_semester => '2017 Fall',
+#                       :course_id => '026', :section_id => '101', :notes => 'I am interested in CS026 as it is my field of research.' ,:priority => 'Very High', :state => 'Active'},
+#                       {:uin => '225008989', :name => 'Shubham Jain', 
+#                       :major => 'CPSC', :classification => 'G8', :minor => 'PHYS', :email => 'shubham7jain@tamu.edu', :phone => '9792377979', 
+#                       :expected_graduation => '2017 Fall', :request_semester => '2017 Spring',
+#                       :course_id => '028', :section_id => '101', :notes => 'I need this subject to graduate.',:priority => 'Very High' , :state => 'Active'},
+#                       {:uin => '525008931', :name => 'Aditya Nanjangud', 
+#                      :major => 'CPSC', :classification => 'G8', :minor => 'CECN', :email => 'aditya@gmail.com', :phone => '9797797697', 
+#                      :expected_graduation => '2018 Spring', :request_semester => '2017 Fall',
+#                      :course_id => '026', :section_id => '101', :notes => 'I need this course since this is my last semester',:priority => 'Very High' , :state => 'Active'},
+#                      {:uin => '525008931', :name => 'Aditya Nanjangud', 
+#                      :major => 'CPSC', :classification => 'G8', :minor => 'CECN', :email => 'aditya@gmail.com', :phone => '9797797697', 
+#                      :expected_graduation => '2018 Spring', :request_semester => '2017 Fall',
+#                      :course_id => '027', :section_id => '101', :notes => 'I need this course since this is my last semester',:priority => 'Very High' , :state => 'Active'},
+#                      {:uin => '525008931', :name => 'Aditya Nanjangud', 
+#                      :major => 'CPSC', :classification => 'G8', :minor => 'CECN', :email => 'aditya@gmail.com', :phone => '9797797697', 
+#                      :expected_graduation => '2018 Spring', :request_semester => '2017 Fall',
+#                      :course_id => '028', :section_id => '101', :notes => 'I need this course since this is my last semester',:priority => 'Very High' , :state => 'Active'},
+#                      {:uin => '525008932', :name => 'Navneet Gupta', 
+#                      :major => 'CPSC', :classification => 'G8', :minor => 'CECN', :email => 'navneet@gmail.com', :phone => '4693889840', 
+#                      :expected_graduation => '2018 Spring', :request_semester => '2017 Fall',
+#                      :course_id => '026', :section_id => '101', :notes => 'I need this course since this is my last semester',:priority => 'High' , :state => 'Active'},
+#                      {:uin => '525008932', :name => 'Navneet Gupta', 
+#                      :major => 'CPSC', :classification => 'G8', :minor => 'CECN', :email => 'navneet@gmail.com', :phone => '4693889840', 
+#                      :expected_graduation => '2018 Spring', :request_semester => '2017 Fall',
+#                      :course_id => '027', :section_id => '101', :notes => 'I need this course since this is my last semester',:priority => 'High' , :state => 'Active'},
+#                      {:uin => '525008932', :name => 'Navneet Gupta', 
+#                      :major => 'CPSC', :classification => 'G8', :minor => 'CECN', :email => 'navneet@gmail.com', :phone => '4693889840', 
+#                      :expected_graduation => '2018 Spring', :request_semester => '2017 Fall',
+#                      :course_id => '028', :section_id => '101', :notes => 'I need this course since this is my last semester',:priority => 'High' , :state => 'Approved'}]
 
 majors = [{:major_id => 'CPSC'}, {:major_id => 'CECN'}, {:major_id => 'CEEN'}, {:major_id => 'ELEN'}, {:major_id => 'APMS'},
             {:major_id => 'CPSL'}, {:major_id => 'CECL'}, {:major_id => 'CEEL'}, {:major_id => 'Others'}]
             
-# admins = [{:uin => '123'}, {:uin => '123456789'}, {:uin => '456789123'}, {:uin => '987654321'}]
-admins = [{:uin => '123', :name => 'admin', :password => '123'}]
+admins = [{:uin => '123456789', :name => 'admin', :password => 'tamu2017'}]
 
-students = [{uin: "126003824", password: "123123", major: "Computer Engineering - CEEN", classification: "G7-Graduate, Master's Level", name: "Mo Li", email: "king_lm@tamu.edu"}]
+# students = [{uin: "126003824", password: "123123", major: "Computer Engineering - CEEN", classification: "G7-Graduate, Master's Level", name: "Mo Li", email: "king_lm@tamu.edu"}]
 
-students.each do |student|
-   Student.create!(student) 
-end
+# students.each do |student|
+#   Student.create!(student) 
+# end
 
-student_requests.each do |record|
-  StudentRequest.create!(record)
-end
+# student_requests.each do |record|
+#   StudentRequest.create!(record)
+# end
 
 majors.each do |record|
   Major.create!(record)
