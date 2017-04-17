e12b52d943189fb2751f03912d5b329d96cb7227
Mo Li <limo9373@gmail.com>
2017-04-17 15:17:57 +0000

    fix bug for reset pw

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,8 +65,8 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'cs606-limestone.c9users.io'}#for cloud 9
-  # config.action_mailer.default_url_options = { host: 'cse-force-request-system.herokuapp.com'}#for heroku
+  # config.action_mailer.default_url_options = { host: 'cs606-limestone.c9users.io'}#for cloud 9
+  config.action_mailer.default_url_options = { host: 'cse-force-request-system.herokuapp.com'}#for heroku
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
