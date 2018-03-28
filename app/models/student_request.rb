f5f70c0c5c69db75a101c593e6aab97992c8708b
Andrew Bregger <adb3649@tamu.edu>
2018-03-28 21:05:07 +0000

    Updating files

--- app/models/student_request.rb
+++ app/models/student_request.rb
@@ -32,7 +32,7 @@ class StudentRequest < ActiveRecord::Base
     current_month = CURRENT_MONTH.to_i
     
     LIST_YEAR = []
-    for i in current_year..current_year+10
+    for i in current_year-100..current_year+100
       LIST_YEAR << i.to_s
     end
     
