4f52b107a691217d1633a39d18ffe95815cf4435
JoshuaHowell <howjosh@gmail.com>
2018-04-01 18:55:18 -0500

    JH -- Admin Test -- Complete testing for Admin test.

--- spec/controllers/admins_controller_spec.rb
+++ spec/controllers/admins_controller_spec.rb
@@ -18,11 +18,18 @@ describe AdminsController, :type => :controller do
        end
 
        context 'when the incorrect params are passed in' do
+         before :each do
+           post :addAdmin, :admin_request => {:wrong_params => "No, really. The wrong params"}
+         end
+
          it 'render a flash notice' do
+           expect(flash[:warning]).to eq("Name can't be blank,Name is invalid,Uin can't be blank,Password can't be blank")
 
          end
 
-         it 'should redirec to the advmin review path'
+         it 'should redirec to the advmin review path' do
+           assert_response :redirect, :action => 'student_requests_adminview_path'
+         end
        end
 
     end
