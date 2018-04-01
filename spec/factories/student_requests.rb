8c49a5469b73f938519676d2a808da8c3caaa8a8
JoshuaHowell <howjosh@gmail.com>
2018-04-01 16:15:38 -0500

    JH -- RSpec Testing -- Fix student_controller_spec tests.

--- spec/factories/student_requests.rb
+++ spec/factories/student_requests.rb
@@ -1,12 +1,12 @@
 # This will guess the User class
 FactoryGirl.define do
   factory :student_request do
-    full_name "John Doe"
+    name "John Doe"
     uin "345345345"
     major "CSCE"
     classification "G8"
     email "johndoe@tamu.edu"
-    request_semester "2017 Spring"
+    request_semester "2019 Spring"
     course_id "633"
     phone "9793459799"
   end
