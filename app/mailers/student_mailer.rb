8fcd541339e07dbd9200806c9e58b535583ccb4d
JoshuaHowell <howjosh@gmail.com>
2018-04-01 17:17:59 -0500

    JH -- Testing -- Add tests around registration confirmation for the student mailer.

--- app/mailers/student_mailer.rb
+++ app/mailers/student_mailer.rb
@@ -6,15 +6,15 @@ class StudentMailer < ActionMailer::Base
         # mail(:to => "#{student.name} <#{student.email}>", :subject => "Registration Confirmation")
         mail(:to => @student.email, :subject => "Registration Confirmation")
     end
-    
+
     def confirm_force_request(student, req)
         @student = student
         @req = req
         mail(:to => @student.email, :subject => "Request Confirmation")
     end
-    
+
     def reset_password(student)
         @student = student
         mail(:to => @student.email, :subject => "Reset Your Password")
     end
-end 
+end
--- spec/mailers/student_mailer_spec.rb
+++ spec/mailers/student_mailer_spec.rb
@@ -6,16 +6,25 @@ require 'spec_helper'
 RSpec.describe StudentMailer, type: :mailer do
   describe 'registration_confirmation' do
     #let(:student) {mock_model Student, name: 'Bernard Lowe', email: 'blowe@westworld.com', password: 'IAmArnorld', uin: 12345678}
-    student = Student.new( name: 'Bernard Lowe', email: 'blowe@westworld.com', password: 'IAmArnorld', uin: 12345678)
-    student.save
-    let(:mail) { described_class.registration_confirmation(student).deliver_now }
+
+    before :each do
+      @student = Student.new( name: 'Bernard Lowe', email: 'blowe@westworld.com', password: 'IAmArnorld', uin: 12345678)
+      @student.save
+    end
+
+    let(:mail) { described_class.registration_confirmation(@student).deliver_now }
 
     it 'renders the subject' do
 
 
-      StudentMailer.registration_confirmation(student)
+      StudentMailer.registration_confirmation(@student)
       expect(mail.subject).to eq('Registration Confirmation')
-      student.destroy
+      expect(mail.to).to eq(['blowe@westworld.com'])
+
+    end
+
+    after :each do
+      @student.destroy
     end
 
     # it 'should send an email' do
