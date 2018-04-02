2b93c2f33e2e5771bd7fe9de24b082b5223a17dc
JoshuaHowell <howjosh@gmail.com>
2018-04-02 11:25:48 -0500

    JH -- Student Request Controller Testing -- Add test around create for Student Controller Test.

--- spec/controllers/student_request_controller_spec.rb
+++ spec/controllers/student_request_controller_spec.rb
@@ -3,30 +3,50 @@ require 'rails_helper'
 
 describe StudentRequestsController, :type => :controller do
   describe "Create Student Request: " do
-    it 'creates a student request' do
+    context 'on properly formatted create request' do
+      it 'creates a student request' do
 
-      #Given
-      student = FactoryGirl.create(:student)
-      student_request = FactoryGirl.create(:student_request)
-      Student.should_receive(:where).once.and_return([student])
-      StudentMailer.should_receive(:confirm_force_request).once.and_return( double("Mailer", :deliver => true) );
-
-
-      #When
-      post :create, :student_request => {:name => student_request.name,
-                                         :uin => student_request.uin,
-                                         :major => student_request.major,
-                                         :classification => student_request.classification,
-                                         :email => student_request.email,
-                                         :request_semester => student_request.request_semester,
-                                         :course_id => student_request.course_id,
-                                         :phone => student_request.phone}
-
-      #Then
-      expect(flash[:notice]).to eq("Student Request was successfully created.")
-      assert_response :redirect, :action => 'students_show_path'
+        #Given
+        student = FactoryGirl.create(:student)
+        student_request = FactoryGirl.create(:student_request)
+        Student.should_receive(:where).once.and_return([student])
+        StudentMailer.should_receive(:confirm_force_request).once.and_return( double("Mailer", :deliver => true) );
 
 
+        #When
+        post :create, :student_request => {:name => student_request.name,
+                                           :uin => student_request.uin,
+                                           :major => student_request.major,
+                                           :classification => student_request.classification,
+                                           :email => student_request.email,
+                                           :request_semester => student_request.request_semester,
+                                           :course_id => student_request.course_id,
+                                           :phone => student_request.phone}
+
+        #Then
+        expect(flash[:notice]).to eq("Student Request was successfully created.")
+        assert_response :redirect, :action => 'students_show_path'
+
+      end
+    end
+
+    context 'on mal formatted create request' do
+      it 'attempts to create a new a New Force Request' do
+
+        #Given
+        student = FactoryGirl.create(:student)
+        student_request = FactoryGirl.create(:student_request)
+        Student.should_receive(:where).once.and_return([student])
+
+
+        #When
+        post :create, :student_request => {:name => student_request.name}
+
+
+        #Then
+        expect(flash[:warning]).to eq("Uin can't be blank, Major can't be blank, Classification can't be blank, Request semester can't be blank, Request semester  is not a valid request semester, Course can't be blank, Course is invalid")
+        assert_template 'new'
+      end
     end
   end
 end
